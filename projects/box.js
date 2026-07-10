ac3ef7ddd7f113df4d670c855618b418283b4e59
Rishabh Dubey <rishabhdubey@192.168.68.109>
2026-07-10 10:45:17 +0530

    box.js

---
+++ projects/box.js
@@ -0,0 +1,18 @@
+
+
+const btn =  document.getElementById("btn")
+const container = document.getElementById("container")
+
+btn.addEventListener("click", ()=>{
+
+    const box =document.createElement("div")
+
+    box.classList.add("box")
+
+    container.appendChild(box)
+
+
+    }
+
+)
+
